a3882cbf133090f722a8f62f2bd9aa3ca547e4b3
MAtanante <marck.atanante@telus.com>
2023-06-18 14:10:54 +0800

    fix white space of cards

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -4,13 +4,13 @@ import Trash from './icons/Trash'
 
 const UserCard = ({user = {}, onUserDelete})=> {
     return (
-        <div className='group transition-all scale-95 hover:scale-105 hover:z-10 hover:bg-gray-100 hover:drop-shadow-lg relative px-2 py-1 rounded border flex flex-col shrink-0 basis-1/3' key={user.id}>
-            <h5 className="text-xl mb-3">{user.name}</h5>
-            <div className="flex text-sm text-gray-500">
+        <div className='group px-4 py-2 transition-all scale-95 hover:scale-105 hover:z-10 hover:bg-gray-100 hover:drop-shadow-lg relative px-2 py-1 rounded border flex flex-col shrink-0 basis-1/3' key={user.id}>
+            <h5 className="text-xl mt-3 mb-2">{user.name}</h5>
+            <div className="flex items-center text-sm text-gray-500 mb-1">
                 <Mail/>
                 <span className="ml-2">{user.email}</span>
             </div>
-            <div className="flex text-sm text-gray-500">
+            <div className="flex items-center text-sm text-gray-500">
                 <Telephone/>
                 <span className="ml-2">{user.phone}</span>
             </div>
