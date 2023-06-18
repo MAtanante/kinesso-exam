6c97b752d676d69f008c80b3c829af2e80db3650
MAtanante <marck.atanante@telus.com>
2023-06-19 06:22:10 +0800

    Added const var for user URL

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+
 export const fetchUsers = () => {
     return async(dispatch)=> {
         try {
             dispatch({type: 'GET_USERS_INIT'})
-            const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+            const res = await axios.get(USERS_URL)
             dispatch({type: 'GET_USERS_SUCCESS', payload: res.data})
         } catch (error) {
             dispatch({type: 'GET_USER_ERROR', error})
