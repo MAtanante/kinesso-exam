a5fb4986d8d0fdb1bddaa56d0f02a929135cc872
MAtanante <marck.atanante@telus.com>
2023-06-18 12:41:49 +0800

    Added fetch status

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { fetchUsers } from "../actions/userActions"
+import {fetchUsers} from '../actions/user'
 
 const Users =()=> {
     const dispatch = useDispatch()
@@ -8,17 +8,17 @@ const Users =()=> {
     useEffect(()=> {
         dispatch(fetchUsers())
     }, [dispatch])
-    const {users} = useSelector(state => state)
-    const status = 'ok'
 
-    console.log('users', users)
+    const users = useSelector(state => state.users)
+    const usersStatus = useSelector(state => state.usersStatus)
 
     return <>
         <h1 className='font-bold my-8 '>USERS</h1>
 
         <div className='flex justify-center'>
         <div className='flex flex-wrap'>
-            {status === 'ok' && users && users.length > 0 && <>
+            {usersStatus === 'loading' &&  <div>Loading</div>}
+            {usersStatus === 'ok' && users && users.length > 0 && <>
             {users.map(user => 
                 <div className='w-1/4 px-2 py-1 rounded border flex flex-col' key={user.id}>
                 <span>{user.name}</span>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,31 @@
 const initState = {
-    users: []
+    users: [],
+    usersStatus: 'idle'
 }
 
 
 const rootReducer = (state = initState, action)=> {
     console.log('action: ', action)
 
-    if(action.type === 'GET_USERS') {
+    if(action.type === 'GET_USERS_INIT') {
         return {
             ...state,
+            usersStatus: 'loading'
+        }
+    }
+
+    if(action.type === 'GET_USERS_SUCCESS') {
+        return {
+            ...state,
+            usersStatus: 'ok',
+            users: action.payload
+        }
+    }
+
+    if(action.type === 'GET_USERS_ERROR') {
+        return {
+            ...state,
+            usersStatus: 'error',
             users: action.payload
         }
     }
